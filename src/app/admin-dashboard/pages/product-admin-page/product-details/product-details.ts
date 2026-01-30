730104f475718ed1b96aeeb2eefcac89e19c4dc7
Omar Enrique <omar.e.geek@gmail.com>
2026-01-30 00:20:08 -0400

    Final de la seccion 21

---
+++ src/app/admin-dashboard/pages/product-admin-page/product-details/product-details.ts
@@ -0,0 +1,83 @@
+import { Component, inject, input, OnInit, signal } from '@angular/core';
+import { Product } from '@products/interfaces/products-response.interface';
+import { ProductCarousel } from '@products/components/product-carousel/product-carousel';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormUtils } from '@utils/form-utils';
+import { FormErrorLabel } from '@shared/components/form-error-label/form-error-label';
+import { ProductsService } from '@products/services/products.service';
+import { Router } from '@angular/router';
+import { firstValueFrom } from 'rxjs';
+
+@Component({
+  selector: 'product-details',
+  imports: [ProductCarousel, ReactiveFormsModule, FormErrorLabel],
+  templateUrl: './product-details.html',
+})
+export class ProductDetails implements OnInit {
+  product = input.required<Product>();
+  productService = inject(ProductsService);
+  router = inject(Router);
+  wasSaved = signal(false);
+
+  fb = inject(FormBuilder);
+  productForm = this.fb.group({
+    title: ['', Validators.required],
+    description: ['', Validators.required],
+    slug: ['', [Validators.required, Validators.pattern(FormUtils.slugPattern)]],
+    price: [0, [Validators.required, Validators.min(0)]],
+    stock: [0, [Validators.required, Validators.min(0)]],
+    sizes: [['']],
+    images: [[]],
+    tags: [''],
+    gender: ['men', [Validators.required, Validators.pattern(/men|women|kid|unisex/)]],
+  });
+
+  sizes = ['XS', 'S', 'M', 'L', 'XL', 'XXL'];
+
+  ngOnInit(): void {
+    // this.productForm.reset(this.product() as any);
+    this.setFormValue(this.product());
+  }
+
+  setFormValue(formLike: Partial<Product>) {
+    this.productForm.reset(this.product() as any);
+    this.productForm.patchValue({ tags: formLike.tags?.join(',') });
+    // this.productForm.patchValue(formLike as any);
+  }
+
+  onSizeClicked(size: string) {
+    const currentSizes = this.productForm.value.sizes ?? [];
+    if (currentSizes.includes(size)) {
+      currentSizes.splice(currentSizes.indexOf(size), 1);
+    } else {
+      currentSizes.push(size);
+    }
+
+    this.productForm.patchValue({ sizes: currentSizes });
+  }
+
+  async onSubmit() {
+    const isValid = this.productForm.valid;
+    this.productForm.markAllAsTouched();
+    if (!isValid) return;
+    const formValue = this.productForm.value;
+    const productLike: Partial<Product> = {
+      ...(formValue as any),
+      tags:
+        formValue.tags
+          ?.toLowerCase()
+          .split(',')
+          .map((tag) => tag.trim()) ?? [],
+    };
+
+    if (this.product().id === 'new') {
+      // Crear producto
+      const product = await firstValueFrom(this.productService.createProduct(productLike));
+      this.router.navigate(['/admin/products', product.id]);
+    } else {
+      await firstValueFrom(this.productService.updateProduct(this.product().id, productLike));
+    }
+    this.wasSaved.set(true);
+    setTimeout(() => this.wasSaved.set(false), 3000);
+  }
+}
